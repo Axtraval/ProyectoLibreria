504dc23e558f09618734ce9e64857851892ed6e6
Alex Omar Chablé Acosta <ao6585051@gmail.com>
2026-02-20 10:01:27 -0600

    cambio de prueba

--- src/main/java/com/proyecto/proyectolibreria/modolectura/Lectura.java
+++ src/main/java/com/proyecto/proyectolibreria/modolectura/Lectura.java
@@ -3,7 +3,7 @@ package com.proyecto.proyectolibreria.modolectura;
 public class Lectura extends Libro{
     public Lectura(String titulo, String autor, String genero, int precio, int anio){
         super(titulo, autor, genero, precio, anio);
-
+//
     }
 
 }
