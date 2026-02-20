d059ec95215d1a4f75401117b3f481aaec025409
dsh1117 <dashaguzman800@gmail.com>
2026-02-20 10:56:30 -0600

    Crud lector

---
+++ src/main/java/com/proyecto/proyectolibreria/Crudlect/Libro.java
@@ -0,0 +1,21 @@
+package com.proyecto.proyectolibreria.Crudlect;
+
+public class Libro {
+    private int num;
+    private String titulo;
+
+    public Libro(int num, String titulo) {
+        this.num = num;
+        this.titulo = titulo;
+    }
+
+
+    public int getNum() { return num; }
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    @Override
+    public String toString() {
+        return "Número: " + num + " | Título: " + titulo;
+    }
+}
