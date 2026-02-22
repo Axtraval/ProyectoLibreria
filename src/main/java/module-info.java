88da8bee52b9dd77ba1724992947192963498e00
RMKprojector.exe <tonkasfrcj@gmail.com>
2026-02-21 21:38:22 -0600

    estoq

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.proyecto.proyectolibreria {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.proyecto.proyectolibreria to javafx.fxml;
+    exports com.proyecto.proyectolibreria;
 
     opens com.proyecto.proyectolibreria.Crudlect to javafx.fxml;
     exports com.proyecto.proyectolibreria.Crudlect;
