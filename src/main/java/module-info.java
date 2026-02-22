ce9f5e9f49aecc6027f25e1cbf3355af39e939e5
Alex Omar Chablé Acosta <ao6585051@gmail.com>
2026-02-21 22:45:09 -0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.proyecto.proyectolibreria {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.proyecto.proyectolibreria to javafx.fxml;
-    exports com.proyecto.proyectolibreria;
+    opens com.proyecto.crud_ingreso to javafx.fxml;
+    exports com.proyecto.crud_ingreso;
+
+    opens com.proyecto.crud_ingreso.controller to javafx.fxml;
 
     opens com.proyecto.proyectolibreria.Crudlect to javafx.fxml;
     exports com.proyecto.proyectolibreria.Crudlect;
